14cb71a80560f7c85d2a284717e6b40a796efcef
SUHAN N <151826206+SuhanN5400@users.noreply.github.com>
2024-06-28 18:17:56 +0530

    Flatten a Nested List.py

---
+++ Flatten a Nested List.py
@@ -0,0 +1,11 @@
+def flatten(nested_list):
+    flat_list = []
+    for item in nested_list:
+        if isinstance(item, list):
+            flat_list.extend(flatten(item))
+        else:
+            flat_list.append(item)
+    return flat_list
+
+nested_list = [1, [2, 3], [4, [5, 6]], 7]
+print("Flattened list:", flatten(nested_list))
