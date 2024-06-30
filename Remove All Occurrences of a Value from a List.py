606e65a7971b1ed7324920d69b73c577934a7f43
SUHAN N <151826206+SuhanN5400@users.noreply.github.com>
2024-06-30 19:10:55 +0530

    Remove All Occurrences of a Value from a List.py

---
+++ Remove All Occurrences of a Value from a List.py
@@ -0,0 +1,5 @@
+my_list = [1, 2, 3, 2, 4, 2, 5]
+value_to_remove = 2
+filtered_list = [x for x in my_list if x != value_to_remove]
+print("List after removing", value_to_remove, ":", filtered_list)
+``
